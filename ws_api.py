f279d7e48d07e6f7679f9744a07bdcce4a4a61a0
Kermut572 <alexandre.navez@pm.me>
2026-04-23 10:28:01 +0000

    fix unit problem

--- ws_api.py
+++ ws_api.py
@@ -934,13 +934,16 @@ async def ws_get_carbon_by_room_with_usage(
             continue
 
         days_from_installation = max(len(consumption_timestamps), 1)
-        usage_carbon_value = sum(
-            ct.get("consumption_footprint", 0.0) for ct in consumption_timestamps
+        usage_carbon_value = (
+            sum(ct.get("consumption_footprint", 0.0) for ct in consumption_timestamps)
+            / 1000
         )
 
         predicted_usage_carbon_value = (
-            usage_carbon_value / days_from_installation
-        ) * device_info.get("lifetime_years" * 365, 1825)  # 1825 days for five years
+            (usage_carbon_value / days_from_installation)
+            * device_info.get("lifetime_years" * 365, 1825)
+            / 1000
+        )  # 1825 days for five years
 
         # Try to find the room
         room_name = "Unknown Room"
@@ -1160,10 +1163,13 @@ async def ws_get_carbon_by_type_with_usage(
         usage_carbon_value = sum(
             ct.get("consumption_footprint", 0.0) for ct in consumption_timestamps
         )
+        usage_carbon_value /= 1000
 
         predicted_usage_carbon_value = (
-            usage_carbon_value / days_from_installation
-        ) * device_info.get("lifetime_years" * 365, 1825)  # 1825 days for five years
+            (usage_carbon_value / days_from_installation)
+            * device_info.get("lifetime_years" * 365, 1825)
+            / 1000
+        )  # 1825 days for five years
 
         embodied_carbon_value = device_info.get("carbon_footprint", 0)
         device_type = device_info.get("type", "Unknown")
