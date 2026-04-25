c67239fbf9cc531dd6fff48e585d852983744bd2
Kermut572 <alexandre.navez@pm.me>
2026-04-25 13:27:01 +0000

    more robust yearly contrib system

--- ws_api.py
+++ ws_api.py
@@ -1443,6 +1443,11 @@ async def ws_get_yearly_contribution(
     for device_id, device_stats in devices.items():
         if energy_meter and device_id == energy_meter:
             continue
+
+        device_energy_entity = device_stats.get("energy_entity", None)
+        if device_energy_entity is None:
+            continue
+
         device_metadata = device_stats.get("metadata", {})
         total_energy_consumed += device_metadata.get("total_energy", 1.0)
 
