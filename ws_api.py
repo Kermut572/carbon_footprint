ab7163bdd51d1becaccf92c13ab5bee686c8b369
Kermut572 <alexandre.navez@pm.me>
2026-05-01 10:52:01 +0000

    fix reset sensors websocket

--- ws_api.py
+++ ws_api.py
@@ -1804,12 +1804,14 @@ async def ws_reset_sensors(
         return
 
     device_name = device_info.get("display_name", "err")
-    iot_sensor = device_info.get("cu_entity", None)
-    app_sensor = device_info.get("cu_app_entity", None)
+    iot_sensor = device_info.get("cu_entity", "cf_err_no_device")
+    app_sensor = device_info.get("cu_app_entity", "cf_err_no_device")
 
-    iot_sensor_entity = None
-    if iot_sensor is not None:
-        iot_sensor_entity = entity_reg.async_get_entity_id("sensor", DOMAIN, iot_sensor)
+    iot_sensor_entry = entity_reg.async_get(iot_sensor)
+    if iot_sensor_entry is not None:
+        iot_sensor_entity = entity_reg.async_get_entity_id(
+            "sensor", DOMAIN, iot_sensor_entry.entity_id
+        )
         if iot_sensor_entity is not None:
             _LOGGER.debug(
                 "Removing %s sensor and history from device %s", iot_sensor, device_name
@@ -1822,9 +1824,11 @@ async def ws_reset_sensors(
             entity_reg.async_remove(iot_sensor_entity)
             device_info["cu_entity"] = ""
 
-    app_sensor_entity = None
-    if app_sensor is not None:
-        app_sensor_entity = entity_reg.async_get_entity_id("sensor", DOMAIN, app_sensor)
+    app_sensor_entry = entity_reg.async_get(app_sensor)
+    if app_sensor_entry is not None:
+        app_sensor_entity = entity_reg.async_get_entity_id(
+            "sensor", DOMAIN, app_sensor_entry.entity_id
+        )
         if app_sensor_entity is not None:
             _LOGGER.debug(
                 "Removing %s sensor and history from device %s", app_sensor, device_name
