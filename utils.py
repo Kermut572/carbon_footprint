b5b28cf63fa3ebe02c64a635ce3d3f22a9b979ac
Kermut572 <alexandre.navez@pm.me>
2026-05-01 14:06:21 +0000

    fix debug log placements and added more to debug

--- utils.py
+++ utils.py
@@ -451,12 +451,19 @@ async def utils_get_device_energy_consumption_map(
     energy_entity, appliance_entity, _ = utils_find_energy_entity_for_device(
         hass, device_id
     )
-    _LOGGER.debug("No energy entity found for device id %s, skipping", device_id)
     if not energy_entity:
+        _LOGGER.debug("No energy entity found for device id %s, skipping", device_id)
         return None
     if is_appliance and not appliance_entity:
         _LOGGER.debug("No appliance entity found for device id %s, skipping", device_id)
         return None
+
+    if is_appliance:
+        _LOGGER.debug(
+            "Pulling stats for appliance for device id %s, appliance entity %s",
+            device_id,
+            appliance_entity,
+        )
 
     stats = await recorder.get_instance(hass).async_add_executor_job(
         statistics_during_period,
